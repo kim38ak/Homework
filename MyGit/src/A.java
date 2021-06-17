40c9dafbee16d81318f6827b1045ab2850887407
kimhyunjo <kimhyunjo@218.38.137.28>
2021-06-18 00:42:25 +0900

    Change A class and C class

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,6 +3,9 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("A Class");
+		
+		C c = new C();
+		c.printC();
 
 	}
 
--- MyGit/src/C.java
+++ MyGit/src/C.java
@@ -3,5 +3,6 @@ public class C {
 
 	public void printC() {
 		System.out.println("C Class");
+		System.out.println("CCC");
 	}
 }
